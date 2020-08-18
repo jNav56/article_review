c47f495d493941d60392cf04f6683a4b43c74177
jNav56 <60667487+jNav56@users.noreply.github.com>
2020-08-17 22:42:24 -0500

    sharing woks, no desktop preview or mobile yet

--- script.js
+++ script.js
@@ -11,15 +11,19 @@ shareButton[0].addEventListener("click", function() {
 
 var facebookShare = document.getElementsByClassName("facebook");
 facebookShare[0].addEventListener("click", function() {
-
+    var url = "https://www.facebook.com/sharer.php?href=" + document.location + "&p[title]=" + document.title;
+    window.open(url,'_blank');
+    // alert(url);
 })
 
 var twitterShare = document.getElementsByClassName("twitter");
 twitterShare[0].addEventListener("click", function() {
-
+    var url = "https://twitter.com/intent/tweet?text=" + document.title + "&url=" + document.location;
+    window.open(url,'_blank');
 })
 
 var pinterestShare = document.getElementsByClassName("pinterest");
 pinterestShare[0].addEventListener("click", function() {
-    alert("yo dawg");
+    var url = "http://pinterest.com/pin/create/button/?url=" + document.location + "&description=" + document.title;
+    window.open(url,'_blank');
 })
